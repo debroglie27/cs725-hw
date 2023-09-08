340ec141f1fff925f92df9c5d3b0831f6cd08c57
Arijeet De <arijeetde@gmail.com>
2023-09-08 23:38:35 +0530

    fixing some warnings

--- hw3/model.py
+++ hw3/model.py
@@ -18,14 +18,6 @@ class NaiveBayes:
         You can create a separate function for fitting each distribution in its and call it here.
         """
 
-
-
-
-
-
-
-
-
         """End of your code."""
 
     def predict(self, X):
@@ -39,12 +31,6 @@ class NaiveBayes:
         It is implied that prediction[i] is the class that maximizes posterior probability for ith data point in X.
         You can create a separate function for calculating posterior probability and call it here.
         """
-
-
-
-
-
-
 
         """End of your code."""
 
@@ -61,23 +47,19 @@ class NaiveBayes:
         multinomial = {"0":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"1":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"2":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]]}
         """
         priors = {}
-        guassian = {}
+        gaussian = {}
         bernoulli = {}
         laplace = {}
         exponential = {}
         multinomial = {}
 
         """Start your code"""
-
-
-
-
         
         """End your code"""
-        return (priors, guassian, bernoulli, laplace, exponential, multinomial)        
+        return priors, gaussian, bernoulli, laplace, exponential, multinomial
 
 
-def save_model(model,filename="model.pkl"):
+def save_model(model, filename="model.pkl"):
     """
 
     You are not required to modify this part of the code.
@@ -87,18 +69,20 @@ def save_model(model,filename="model.pkl"):
     pkl.dump(model,file)
     file.close()
 
+
 def load_model(filename="model.pkl"):
     """
 
     You are not required to modify this part of the code.
 
     """
-    file = open(filename,"rb")
+    file = open(filename, "rb")
     model = pkl.load(file)
     file.close()
     return model
 
-def visualise(data_points,labels):
+
+def visualise(data_points, labels):
     """
     datapoints: np.array of shape (n,2)
     labels: np.array of shape (n,)
@@ -138,13 +122,7 @@ def net_f1score(predictions, true_labels):
         """
         """Start of your code."""
         
-
-
-
-        
         """End of your code."""
-        
-
 
     def recall(predictions, true_labels, label):
         """Calculate the multclass recall of the predictions.
@@ -157,16 +135,11 @@ def net_f1score(predictions, true_labels):
             float: The recall of the predictions.
         """
         """Start of your code."""
-        
-
-
-
 
         """End of your code."""
-        
 
     def f1score(predictions, true_labels, label):
-        """Calculate the f1 score using it's relation with precision and recall.
+        """Calculate the f1 score using its relation with precision and recall.
 
         Args:
             predictions (np.array): The predicted labels.
@@ -177,28 +150,23 @@ def net_f1score(predictions, true_labels):
         """
 
         """Start of your code."""
-        
-
-
-
 
         """End of your code."""
         return f1
-    
 
     f1s = []
     for label in np.unique(true_labels):
         f1s.append(f1score(predictions, true_labels, label))
     return f1s
 
-def accuracy(predictions,true_labels):
+
+def accuracy(predictions, true_labels):
     """
 
     You are not required to modify this part of the code.
 
     """
-    return np.sum(predictions==true_labels)/predictions.size
-
+    return np.sum(predictions == true_labels)/predictions.size
 
 
 if __name__ == "__main__":
@@ -209,11 +177,11 @@ if __name__ == "__main__":
     """
 
     # Load the data
-    train_dataset = pd.read_csv('./data/train_dataset.csv',index_col=0).to_numpy()
-    validation_dataset = pd.read_csv('./data/validation_dataset.csv',index_col=0).to_numpy()
+    train_dataset = pd.read_csv('./data/train_dataset.csv', index_col=0).to_numpy()
+    validation_dataset = pd.read_csv('./data/validation_dataset.csv', index_col=0).to_numpy()
 
     # Extract the data
-    train_datapoints = train_dataset[:,:-1]
+    train_datapoints = train_dataset[:, :-1]
     train_labels = train_dataset[:, -1]
     validation_datapoints = validation_dataset[:, 0:-1]
     validation_labels = validation_dataset[:, -1]
@@ -248,4 +216,3 @@ if __name__ == "__main__":
 
     # Visualize the predictions
     # visualise(validation_datapoints, validation_predictions, "validation_predictions.png")
-
