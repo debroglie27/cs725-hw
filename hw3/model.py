31d1e789cb4472b961aa21c6f99bb0a04ed67087
Arijeet De <arijeetde@gmail.com>
2023-09-14 12:24:29 +0530

    evaluate submission

--- hw3/model.py
+++ hw3/model.py
@@ -26,68 +26,17 @@ class NaiveBayes:
         You can create a separate function for fitting each distribution in its and call it here.
         """
 
-        self.priors, self.gaussian, self.bernoulli, self.laplace, self.exponential, self.multinomial = self.get_params(x, y)
-        """End of your code."""
-
-    def predict(self, x):
-        """Start of your code."""
-        """
-        x : np.array of shape (n,10)
-
-        Calculate the posterior probability using the parameters of the distribution calculated in fit function.
-        Take care of underflow errors suitably (Hint: Take log of probabilities)
-        Return an np.array() of predictions where predictions[i] is the predicted class for ith data point in X.
-        It is implied that prediction[i] is the class that maximizes posterior probability for ith data point in X.
-        You can create a separate function for calculating posterior probability and call it here.
-        """
-        predictions = []
-        for label in [0, 1, 2]:
-            px1 = -0.5 * np.log(2 * np.pi * self.gaussian[label][2]) - 0.5 * (((x.T[0] - self.gaussian[label][0])**2) / self.gaussian[label][2])
-            px2 = -0.5 * np.log(2 * np.pi * self.gaussian[label][3]) - 0.5 * (((x.T[1] - self.gaussian[label][1])**2) / self.gaussian[label][3])
-
-            px3 = x.T[2]*np.log(self.bernoulli[label][0]) + (1 - x.T[2])*np.log(1 - self.bernoulli[label][0])
-            px4 = x.T[3]*np.log(self.bernoulli[label][1]) + (1 - x.T[3])*np.log(1 - self.bernoulli[label][1])
-
-            px5 = -np.log(2 * self.laplace[label][2]) - ((np.absolute(x.T[4] - self.laplace[label][0])) / self.laplace[label][2])
-            px6 = -np.log(2 * self.laplace[label][3]) - ((np.absolute(x.T[5] - self.laplace[label][1])) / self.laplace[label][3])
-
-            px7 = np.log(self.exponential[label][0]) - self.exponential[label][0] * x.T[6]
-            px8 = np.log(self.exponential[label][1]) - self.exponential[label][1] * x.T[7]
-
-            px9 = np.log(np.array(self.multinomial[label][0])[x.T[8].astype(int)])
-            px10 = np.log(np.array(self.multinomial[label][1])[x.T[9].astype(int)])
-
-            p = np.log(self.priors[label]) + px1 + px2 + px3 + px4 + px5 + px6 + px7 + px8 + px9 + px10
-
-            predictions.append(p)
-
-        predictions = np.argmax(np.array(predictions).T, axis=1)
-
-        return predictions
-
-    @staticmethod
-    def get_params(x, y):
-        """
-        Return your calculated priors and parameters for all the classes in the form of dictionary that will be used for evaluation
-        Please don't change the dictionary names
-        Here is what the output would look like:
-        priors = {"0":0.2,"1":0.3,"2":0.5}
-        gaussian = {"0":[mean_x1,mean_x2,var_x1,var_x2],"1":[mean_x1,mean_x2,var_x1,var_x2],"2":[mean_x1,mean_x2,var_x1,var_x2]}
-        bernoulli = {"0":[p_x3,p_x4],"1":[p_x3,p_x4],"2":[p_x3,p_x4]}
-        laplace = {"0":[mu_x5,mu_x6,b_x5,b_x6],"1":[mu_x5,mu_x6,b_x5,b_x6],"2":[mu_x5,mu_x6,b_x5,b_x6]}
-        exponential = {"0":[lambda_x7,lambda_x8],"1":[lambda_x7,lambda_x8],"2":[lambda_x7,lambda_x8]}
-        multinomial = {"0":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"1":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"2":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]]}
-        """
-        priors = {}
-        gaussian = {}
-        bernoulli = {}
-        laplace = {}
-        exponential = {}
-        multinomial = {}
+        # self.priors, self.gaussian, self.bernoulli, self.laplace, self.exponential, self.multinomial = self.getParams(x, y)
+        self.priors = {}
+        self.gaussian = {}
+        self.bernoulli = {}
+        self.laplace = {}
+        self.exponential = {}
+        self.multinomial = {}
 
         """Start your code"""
         for label in np.unique(y).astype(int):
-            priors[label] = np.bincount(y.astype(int))[label] / len(x)
+            self.priors[label] = np.bincount(y.astype(int))[label] / len(x)
 
             x_label = x[y == label]
 
@@ -107,14 +56,68 @@ class NaiveBayes:
             lambda_x7 = 1 / np.mean(x_label.T[6])
             lambda_x8 = 1 / np.mean(x_label.T[7])
 
-            gaussian[label] = [mean_x1, mean_x2, var_x1, var_x2]
-            bernoulli[label] = [p_x3, p_x4]
-            laplace[label] = [mu_x5, mu_x6, b_x5, b_x6]
-            exponential[label] = [lambda_x7, lambda_x8]
-            multinomial[label] = [np.bincount(x_label.T[8].astype(int)) / len(x_label),
-                                  np.bincount(x_label.T[9].astype(int)) / len(x_label)]
-        """End your code"""
-        return priors, gaussian, bernoulli, laplace, exponential, multinomial
+            self.gaussian[label] = [mean_x1, mean_x2, var_x1, var_x2]
+            self.bernoulli[label] = [p_x3, p_x4]
+            self.laplace[label] = [mu_x5, mu_x6, b_x5, b_x6]
+            self.exponential[label] = [lambda_x7, lambda_x8]
+            self.multinomial[label] = [np.bincount(x_label.T[8].astype(int)) / len(x_label),
+                                       np.bincount(x_label.T[9].astype(int)) / len(x_label)]
+        """End of your code."""
+
+    def predict(self, x):
+        """Start of your code."""
+        """
+        x : np.array of shape (n,10)
+
+        Calculate the posterior probability using the parameters of the distribution calculated in fit function.
+        Take care of underflow errors suitably (Hint: Take log of probabilities)
+        Return an np.array() of predictions where predictions[i] is the predicted class for ith data point in X.
+        It is implied that prediction[i] is the class that maximizes posterior probability for ith data point in X.
+        You can create a separate function for calculating posterior probability and call it here.
+        """
+        predictions = []
+        for label in [0, 1, 2]:
+            px1 = -0.5 * np.log(2 * np.pi * self.gaussian[label][2]) - 0.5 * (
+                        ((x.T[0] - self.gaussian[label][0]) ** 2) / self.gaussian[label][2])
+            px2 = -0.5 * np.log(2 * np.pi * self.gaussian[label][3]) - 0.5 * (
+                        ((x.T[1] - self.gaussian[label][1]) ** 2) / self.gaussian[label][3])
+
+            px3 = x.T[2] * np.log(self.bernoulli[label][0]) + (1 - x.T[2]) * np.log(1 - self.bernoulli[label][0])
+            px4 = x.T[3] * np.log(self.bernoulli[label][1]) + (1 - x.T[3]) * np.log(1 - self.bernoulli[label][1])
+
+            px5 = -np.log(2 * self.laplace[label][2]) - (
+                        (np.absolute(x.T[4] - self.laplace[label][0])) / self.laplace[label][2])
+            px6 = -np.log(2 * self.laplace[label][3]) - (
+                        (np.absolute(x.T[5] - self.laplace[label][1])) / self.laplace[label][3])
+
+            px7 = np.log(self.exponential[label][0]) - self.exponential[label][0] * x.T[6]
+            px8 = np.log(self.exponential[label][1]) - self.exponential[label][1] * x.T[7]
+
+            px9 = np.log(np.array(self.multinomial[label][0])[x.T[8].astype(int)])
+            px10 = np.log(np.array(self.multinomial[label][1])[x.T[9].astype(int)])
+
+            p = np.log(self.priors[label]) + px1 + px2 + px3 + px4 + px5 + px6 + px7 + px8 + px9 + px10
+
+            predictions.append(p)
+
+        predictions = np.argmax(np.array(predictions).T, axis=1)
+
+        return predictions
+
+    def getParams(self):
+        """
+        Return your calculated priors and parameters for all the classes in the form of dictionary that will be used for evaluation
+        Please don't change the dictionary names
+        Here is what the output would look like:
+        priors = {"0":0.2,"1":0.3,"2":0.5}
+        gaussian = {"0":[mean_x1,mean_x2,var_x1,var_x2],"1":[mean_x1,mean_x2,var_x1,var_x2],"2":[mean_x1,mean_x2,var_x1,var_x2]}
+        bernoulli = {"0":[p_x3,p_x4],"1":[p_x3,p_x4],"2":[p_x3,p_x4]}
+        laplace = {"0":[mu_x5,mu_x6,b_x5,b_x6],"1":[mu_x5,mu_x6,b_x5,b_x6],"2":[mu_x5,mu_x6,b_x5,b_x6]}
+        exponential = {"0":[lambda_x7,lambda_x8],"1":[lambda_x7,lambda_x8],"2":[lambda_x7,lambda_x8]}
+        multinomial = {"0":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"1":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]],"2":[[p0_x9,...,p4_x9],[p0_x10,...,p7_x10]]}
+        """
+
+        return self.priors, self.gaussian, self.bernoulli, self.laplace, self.exponential, self.multinomial
 
 
 def save_model(my_model, filename="model.pkl"):
@@ -186,7 +189,7 @@ def net_f1score(predictions, true_labels):
             if predicted_label == class_label and actual_label != class_label:
                 fp = fp + 1
 
-        return tp/(tp+fp)
+        return tp / (tp + fp)
 
     def recall(predicted_labels, actual_labels, class_label):
         """Calculate the multiclass recall of the predictions.
@@ -221,7 +224,7 @@ def net_f1score(predictions, true_labels):
 
         p = precision(predicted_labels, actual_labels, class_label)
         r = recall(predicted_labels, actual_labels, class_label)
-        f1 = (2*p*r)/(p+r)
+        f1 = (2 * p * r) / (p + r)
 
         return f1
 
@@ -237,7 +240,7 @@ def accuracy(predictions, true_labels):
     You are not required to modify this part of the code.
 
     """
-    return np.sum(predictions == true_labels)/predictions.size
+    return np.sum(predictions == true_labels) / predictions.size
 
 
 if __name__ == "__main__":
